a62bb005a5cf9c3c6f16c5771981b805d6284fb3
ing-lopez <lopezservinalan@yahoo.es>
2019-11-11 20:07:24 -0600

    first commit

---
+++ Examen3/Controllers/EmpleadosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Examen3.Models;
+
+namespace Examen3.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private pruebaEntities1 db = new pruebaEntities1();
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            var empleados = db.Empleados.Include(e => e.Departamentos);
+            return View(empleados.ToList());
+        }
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_departameto = new SelectList(db.Departamentos, "id_departamento", "nombre_departamento");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_empleado,id_departameto,nombre_empleado,Status")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empleados.Add(empleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_departameto = new SelectList(db.Departamentos, "id_departamento", "nombre_departamento", empleados.id_departameto);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_departameto = new SelectList(db.Departamentos, "id_departamento", "nombre_departamento", empleados.id_departameto);
+            return View(empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_empleado,id_departameto,nombre_empleado,Status")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_departameto = new SelectList(db.Departamentos, "id_departamento", "nombre_departamento", empleados.id_departameto);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleados empleados = db.Empleados.Find(id);
+            db.Empleados.Remove(empleados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Examen3/Controllers/NominasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Examen3.Models;
+
+namespace Examen3.Controllers
+{
+    public class NominasController : Controller
+    {
+        private pruebaEntities1 db = new pruebaEntities1();
+
+        // GET: Nominas
+        public ActionResult Index()
+        {
+            var nominas = db.Nominas.Include(n => n.Empleados);
+            return View(nominas.ToList());
+        }
+
+        // GET: Nominas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.Nominas.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nominas);
+        }
+
+        // GET: Nominas/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_empleado = new SelectList(db.Empleados, "id_empleado", "nombre_empleado");
+            return View();
+        }
+
+        // POST: Nominas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_nomina,id_empleado,fecha,Sueldo_por_dia,Dias_laborales,Sueldo_quincenal")] Nominas nominas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Nominas.Add(nominas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_empleado = new SelectList(db.Empleados, "id_empleado", "nombre_empleado", nominas.id_empleado);
+            return View(nominas);
+        }
+
+        // GET: Nominas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.Nominas.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_empleado = new SelectList(db.Empleados, "id_empleado", "nombre_empleado", nominas.id_empleado);
+            return View(nominas);
+        }
+
+        // POST: Nominas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_nomina,id_empleado,fecha,Sueldo_por_dia,Dias_laborales,Sueldo_quincenal")] Nominas nominas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nominas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_empleado = new SelectList(db.Empleados, "id_empleado", "nombre_empleado", nominas.id_empleado);
+            return View(nominas);
+        }
+
+        // GET: Nominas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Nominas nominas = db.Nominas.Find(id);
+            if (nominas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nominas);
+        }
+
+        // POST: Nominas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Nominas nominas = db.Nominas.Find(id);
+            db.Nominas.Remove(nominas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
